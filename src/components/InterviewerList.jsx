11d8918cb114112ba99cb1344d28200900a3eb2b
simplyDonald <donaldabuah@gmail.com>
2021-12-15 18:13:15 -0500

    added proptype library

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types'; 
 
 export default function InterviewerList(props){
   const {interviewers} = props;
@@ -23,3 +24,7 @@ export default function InterviewerList(props){
 
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
